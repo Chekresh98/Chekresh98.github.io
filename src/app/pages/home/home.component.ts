dad2f0fe2dfc2e4e15013559c3158326f946ff59
H Chekresh <hosbectchekresh@gmail.com>
2025-11-08 22:13:36 +0530

    implement custom theme

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  titles = ['frontend', 'backend', 'full stack'];
+  titleNumber = 0;
+  private intervalId: any;
 
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.titleNumber = (this.titleNumber + 1) % this.titles.length;
+    }, 3000); // Change title every 3 seconds
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
